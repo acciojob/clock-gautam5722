1931a2dc1cb3b21c284a20f5f58414b48d207ee3
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2024-12-19 21:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function timerAndDate(){
+	const timerData = document.getElementById('timer');
+	const now = new Date();
+	timerData.textContent = now.toLocaleString();
+}
+
+setInterval(timerAndDate, 1000);
+timerAndDate();
